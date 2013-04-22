4838ceabcfcaec81992239291231d663398f836d
Razzi Abuissa <razzi53@gmail.com>
2013-04-22 03:40:13 -0400

    modified post-update hook to use absolute paths

--- post-update_script.sh
+++ post-update_script.sh
@@ -2,5 +2,5 @@ unset GIT_DIR
 cd ~/cis191-final
 git reset --hard master
 sudo pkill python
-gcc rabuissa.c -o rabuissa.o
+gcc ~/cis191-final/rabuissa.c -o ~/cis191-final/rabuissa.o
 sudo nohup python manage.py runserver 0.0.0.0:80 &
